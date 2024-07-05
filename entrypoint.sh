4327db935727e00a862799993bde6778a75e3c8f
Nadareyn <1404782+Nadareyn@users.noreply.github.com>
2024-07-05 23:33:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,31 @@ set -e
 
 FOLDER=/data
 OWNER_GROUP=sftp
+NEW_KEY=false
 
 # Regenerate keys
-if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+if [ ! -f "/etc/cache_keys/ssh_host_rsa_key" ]; then
+  NEW_KEY=true
   echo "generate rsa key"
-  ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+  ssh-keygen -f /etc/cache_keys/ssh_host_rsa_key -N '' -t rsa
 fi
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+if [ ! -f "/etc/cache_keys/ssh_host_dsa_key" ]; then
+  NEW_KEY=true
   echo "generate dsa key"
-  ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+  ssh-keygen -f /etc/cache_keys/ssh_host_dsa_key -N '' -t dsa
 fi
-if [ ! -f "/etc/ssh/ssh_host_ecdsa_key" ]; then
+if [ ! -f "/etc/cache_keys/ssh_host_ecdsa_key" ]; then
+  NEW_KEY=true
   echo "generate ecdsa key"
-  ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+  ssh-keygen -f /etc/cache_keys/ssh_host_ecdsa_key -N '' -t ecdsa
 fi
+
+if [ $NEW_KEY]; then
+  echo "add volume /etc/cache_keys to avoid new generated keys at startup"
+fi
+
+echo "copy ssh keys"
+cp /etc/cache_keys/* /etc/ssh/
 
 if ! cut -d: -f1 /etc/passwd | grep -q $USERNAME; then
   echo "create user $USERNAME"
@@ -24,41 +35,8 @@ if ! cut -d: -f1 /etc/passwd | grep -q $USERNAME; then
   useradd -M -d $FOLDER -G $OWNER_GROUP $USERNAME
   chown -R $USERNAME:$OWNER_GROUP $FOLDER
 else
-  echo "an user has name $USERNAME"
-  #usermod -u $OWNER_ID -G $OWNER_GROUP -a -d $FOLDER -s /bin/false $USERNAME
+  echo "user $USERNAME already exit"
 fi
-
-# Change sftp password
-echo "$USERNAME:$PASSWORD" | chpasswd
-
-exec "$@"
-
-echo "oh no"
-tail -f /dev/null
-
- # Grab UID of owner of sftp home directory
-  if [ -z $OWNER_ID ]; then
-    OWNER_ID=$(stat -c '%u' $FOLDER)
-  fi
-
-  # Create appropriate SFTP user
-  # If uid doesn't exist on the system
-  if ! cut -d: -f3 /etc/passwd | grep -q $OWNER_ID; then
-    echo "no user has uid $OWNER_ID"
-    # If user doesn't exist on the system
-    if ! cut -d: -f1 /etc/passwd | grep -q $USERNAME; then
-      echo "no user has name $USERNAME"
-      useradd -u $OWNER_ID -M -d $FOLDER -G $OWNER_GROUP -s /bin/false $USERNAME
-    else
-      echo "an user has name $USERNAME"
-      usermod -u $OWNER_ID -G $OWNER_GROUP -a -d $FOLDER -s /bin/false $USERNAME
-    fi
-  else
-    # If user doesn't exist on the system
-    existing_user_with_uid=$(awk -F: "/:$OWNER_ID:/{print \$1}" /etc/passwd)
-    echo "user with uid $OWNER_ID already exist ($existing_user_with_uid)"
-    usermod -d $FOLDER -G $OWNER_GROUP -a -s /bin/false -l $USERNAME $existing_user_with_uid
-  fi
 
 # Change sftp password
 echo "$USERNAME:$PASSWORD" | chpasswd
